6afd442301de0bc8e356097eec02b26344fc6544
Madhav Jha <madhavjha.cse@gmail.com>
2020-12-23 01:17:28 +0530

    first commit

---
+++ src/test/java/com/centralconfig/centralConfigExample/CentralConfigExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centralconfig.centralConfigExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralConfigExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
